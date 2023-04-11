6dea860789f783ffc49e84668ced54ed0f98ab33
saveffer1 <saveffer01@gmail.com>
2023-04-11 17:09:53 +0700

    added center hostname file as json and fetch to read hostname

--- src/script/host.js
+++ src/script/host.js
@@ -1,1 +1,12 @@
-const host = 'http://localhost:8080';
+function getHost() {
+    return fetch('../../host.json')
+    .then(response => response.json())
+    .then(data => JSON.parse(JSON.stringify(data)).hostname)
+    .then(host => host.toString());
+}
+
+let host;
+getHost().then(hostname => {
+    host = hostname;
+    console.log(host);
+});
